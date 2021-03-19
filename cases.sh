e13b31cc052a93ccf8e6057d579da7d2e68fea9e
Kalil Simao <ksekeff@gmail.com>
2021-03-18 21:18:04 -0300

    Criação de Script

---
+++ cases.sh
@@ -0,0 +1,199 @@
+	case $opcao in
+    
+   1) source ./municipios/agua_branca.sh
+     agua_branca
+     menu
+     ;;
+   2) source ./municipios/altos.sh
+     altos
+     menu
+     ;;
+   3) source ./municipios/amarante.sh
+     amarante
+     menu
+     ;;
+   4) source ./municipios/anisiodeabreu.sh
+     anisiodeabreu
+     menu
+     ;;
+   5) source ./municipios/barras.sh
+     barras
+     menu
+     ;;
+   6) source ./municipios/batalha.sh
+     batalha
+     menu
+     ;;
+   7) source ./municipios/bomjesus.sh
+     bomjesus
+     menu
+     ;;
+   8) source ./municipios/buritidoslopes.sh
+     buritidoslopes
+     menu
+     ;;
+   9) source ./municipios/campomaior.sh
+     campomaior
+     menu
+     ;;
+   10) source ./municipios/cantodoburiti.sh
+     cantodoburiti
+     menu
+     ;;	
+   11) source ./municipios/castelodopiaui.sh ##################################################################################
+     castelodopiaui
+     menu
+     ;;
+   12) source ./municipios/cocal.sh
+     cocal
+     menu
+     ;;
+   13) source ./municipios/corrente.sh
+     corrente
+     menu
+     ;;
+   14) source ./municipios/elesbaoveloso.sh
+     elesbaoveloso
+     menu
+     ;;
+   15) source ./municipios/esperantina.sh
+     esperantina
+     menu
+     ;;
+   16) source ./municipios/floriano.sh
+     floriano
+     menu
+     ;;
+   17) source ./municipios/gilbues.sh
+     gilbues
+     menu
+     ;;
+   18) source ./municipios/inhuma.sh
+     inhuma
+     menu
+     ;;
+   19) source ./municipios/jaicos.sh
+     jaicos
+     menu
+     ;;
+   20) source ./municipios/josedefreitas.sh
+     josedefreitas
+     menu
+     ;;
+   21) source ./municipios/luiscorreia.sh
+     luiscorreia
+     menu
+     ;;
+   22) source ./municipios/luzilandia.sh
+     luzilandia
+     menu
+     ;;
+   23) source ./municipios/miguelalves.sh
+     miguelalves
+     menu
+     ;;
+   24) source ./municipios/oeiras.sh
+     oeiras
+     menu
+     ;;
+   25) source ./municipios/palmeirais.sh
+     palmeirais
+     menu
+     ;;
+   26) source ./municipios/parnaiba.sh
+     parnaiba
+     menu
+     ;;
+   27) source ./municipios/paulistana.sh
+     paulistana
+     menu
+     ;;
+   28) source ./municipios/pedroii.sh
+     pedroii
+     menu
+     ;;
+   29) source ./municipios/picos.sh
+     picos
+     menu
+     ;;
+   30) source ./municipios/pioix.sh
+     pioix
+     menu
+     ;;
+   31) source ./municipios/piracuruca.sh
+     piracuruca
+     menu
+     ;;
+   32) source ./municipios/piripiri.sh
+     piripiri
+     menu
+     ;;
+   33) source ./municipios/regeneracao.sh
+     regeneracao
+     menu
+     ;;
+   34) source ./municipios/saojoaodopi.sh
+     saojoaodopi
+     menu
+     ;;
+   35) source ./municipios/saomigueldotapuio.sh
+     saomigueldotapuio
+     menu
+     ;;
+   36) source ./municipios/saopedrodopi.sh
+     saopedrodopi
+     menu
+     ;;
+   37) source ./municipios/saoraimundononato.sh
+     saoraimundononato
+     menu
+     ;;
+   38) source ./municipios/simoes.sh
+     simoes
+     menu
+     ;;
+   39) source ./municipios/simpliciomendes.sh
+     simpliciomendes
+     menu
+     ;;
+   40) source ./municipios/the_centro.sh
+     the_centro
+     menu
+     ;;
+   41) source ./municipios/the_parquepiaui.sh
+     the_parquepiaui
+     menu
+     ;;
+   42) source ./municipios/the_dirceu.sh
+     the_dirceu
+     menu
+     ;;
+   43) source ./municipios/the_espacocidadania.sh
+     the_espacocidadania
+     menu
+     ;;
+   44) source ./municipios/uniao.sh
+     uniao
+     menu
+     ;;
+   45) source ./municipios/urucui.sh
+     urucui
+     menu
+     ;;
+   46) source ./municipios/valencadopi.sh
+     valencadopi
+     menu
+     ;;
+   
+
+   100) echo "Bye Bye"
+	exit 0	
+	;;
+
+   *) echo "Opção Inválida"
+      menu
+      ;;
+	
+	
+	esac
+	
---
+++ municipios/agua_branca.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+agua_branca(){
+                        clear
+                        echo 
+                        echo -n "			
+                        +--------Agua Branca------+        +---------- INFORMAÇÃO --------+              
+                        |1 - Agencia	          |        | Para sair do comando de ping |
+                        |2 - Escritorio           |        | digite CTRL + c e voltara    |
+                        |3 - Subestação           |        | para o menu principal        |
+                        +-------------------------+        +------------------------------+
+                        |4 - Menu                 |                                     
+                        |5 - Sair do programa     |                                     
+                        +-------------------------+       
+                        => "
+                            read escolha
+                            case $escolha in
+                                1) ping 8.8.8.8
+                                ;;
+                                2) ping www.globo.com
+                                ;;
+                                3) ping google.com
+                                ;;
+                                4) menu
+                                ;;
+                                5) echo "Bye bye"
+                                exit 0
+                                ;;
+                            esac
+}
